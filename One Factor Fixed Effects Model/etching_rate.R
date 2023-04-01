bd8908f87473c728cc76c64584503b4b017e9102
AbderrahimZil <128327239+AbderrahimZil@users.noreply.github.com>
2023-03-31 21:54:38 -0400

    update code

--- One Factor Fixed Effects Model/etching_rate.R
+++ One Factor Fixed Effects Model/etching_rate.R
@@ -1,35 +1,5 @@
-# setting the design for for one factor design
-# long.format data
-set.seed(7638)
-f <- factor( rep( c(160,180,200,220), each = 5)) # five replicates are collected
-rate <- sample( f, 20 ) # randomize
-order <- 1:20
-plan <- data.frame( order=order, rate=strength )
 
-# wide format
-set.seed(7638)
-levels <- factor( c(160,180,200,220) ) # five levels are set
-plan <- data.frame(level=levels)
-# write.csv( plan, file = "wide_one_factor.csv", row.names = FALSE)
-# now go and fill the data
-
-# trans.factor <- factor (rep (c("t0", "t12", "t24", "t72"), c(4,3,3,3)))
-
-# replicates <- 5
-# Ys <- data.frame(matrix(NA, nrow = length(levels), ncol = replicates))
-# data.frame( Ys )
-# data.frame( levels=levels)
-# bind(data.frame( levels=levels), data.frame( Ys ))
-
-#names <- c("v","u","w")
-#df <- data.frame(names)
-#for (k in names) df[[k]]<-as.numeric()
-#df
-
-df <- read.csv("plasma_etching.csv")
-str(df)
-
-df 
+## df 
 #|power |  Y1|  Y2|  Y3|  Y4|  Y5|
 #|:-----|---:|---:|---:|---:|---:|
 #|160   | 575| 542| 530| 539| 570|
@@ -41,36 +11,17 @@ df
 df$power = factor(df$power)
 knitr::kable(df)
 
-#???install.packages("reshape2")
+## transform to long format
 library(reshape2)
 df <- melt(df, id.vars = 'power',
            measure.vars = c("Y1", "Y2", "Y3", "Y4", "Y5" ),
            value.name = "rate")
 
-df <- df[-2] # delete measure category Y1 ... Y5
-summary(df)
-
+# visualize
 boxplot(rate ~ power,
         data = df,
         xlab = 'Power (W)',
-        ylab = 'Etch rate (Å/min)')
-# the strength keeps increasing as as content content increases up to 30 %(si) (level)
-# beyond 30% there is a marked decrease in tensil strength
-
-# we wich to test for differences between the mean trength at all the 4 levels of Power 
-# dot plot
-require(ggplot2)
-qplot(df$power,
-           df$rate,
-           xlab = 'Power (W)', ylab = 'Etch rate (Å/min)') + theme_classic()
-
-
-p <- ggplot(df, aes(x=power, y=rate)) +
-  geom_dotplot(binaxis='y', stackdir='center') +
-  theme_classic()
-print(p +labs( title= "", y="Etch rate (Å/min)", x = "Power (W)"))
-
-
+        ylab = 'Etch rate (ï¿½/min)')
 
 # EDA
 library(plyr)
@@ -86,7 +37,8 @@ plot(anova.model)
 par(mfrow=c(1,1))
 
 
-# Posthoc tests
+## Posthoc tests
+# One would like to ...
 library(multcomp)
 mc = glht(model = anova.model, linfct = mcp(power = "Tukey"))
 summary(mc, test=adjusted(type="holm"))
@@ -99,27 +51,17 @@ summary(mc, test=adjusted(type="holm"))
   #200 - 180 == 0    38.00      11.55   3.289  0.00925 ** 
   #220 - 180 == 0   119.60      11.55  10.352 8.47e-08 ***
   #220 - 200 == 0    81.60      11.55   7.063 1.07e-05 ***
-  
-plot(TukeyHSD(anova.model, "power"))
-confint(mc)
-
-plot(mc, main = "",
-     xlim=c(0,160),
-     ylim = c(0.5, 6.0),
-     las=3) # las=2 for x axis
 
 
 ## regression model
 # the experimenter might be interested in knowing ....
-df$power = as.numeric(as.character(df$power))
 linear_fit <- lm(rate ~ poly(power, 2), data = df)
 summary(linear_fit)
 
 
-# prediction
-library(jtools)
+# predictions
 effect_plot(linear_fit, pred = power, interval = TRUE,
             int.type = "confidence", int.width = .8, data = df,
             plot.points = TRUE,
             x.label = 'Power (W)',
-            y.label = 'Etch rate (Å/min)') # note data arg
+            y.label = 'Etch rate (ï¿½/min)') # note data arg
